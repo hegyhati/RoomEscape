c0179f211ff982ad1dbc75e28debac90a5200adb
Mate Hegyhati <hegyhati.work@gmail.com>
2021-09-07 23:11:13 +0200

    Actor base

---
+++ actor.py
@@ -0,0 +1,44 @@
+from map import Map, Position
+
+class DeadActor(Exception):
+    pass
+
+class Actor:
+
+    MOVES = {
+        "forward" : (-1,0),
+        "backward" : (1,0),
+        "left" : (0,-1),
+        "right" : (0,1)
+    }
+
+    map = None
+    position = None
+    
+
+    def set_map(self,map:Map):
+        self.map=map
+    
+    def set_position(self,position:Position):
+        if self.map.get_field(position) in {Map.FREE,Map.GOAL}:
+            self.position=position 
+        else: 
+            raise DeadActor
+
+    def is_alive(self) -> bool:
+        return  self.map.get_field(self.position) in {Map.FREE,Map.GOAL}
+    
+    def is_out(self) -> bool:
+        return  self.map.get_field(self.position) == Map.GOAL
+
+    def _neighbor_index(self, direction:str) -> Position:
+        if direction not in self.MOVES.keys(): raise ValueError
+        return (self.position[0]+self.MOVES[direction][0],self.position[1]+self.MOVES[direction][1])
+    
+    def move(self, direction: str) -> None:
+        if self.is_alive():
+            self.set_position(self._neighbor_index(direction))
+    
+    def can_move(self, direction:str) -> bool:
+        return self.is_alive() and self.map.get_field(self._neighbor_index(direction)) == Map.FREE
+        
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os.path import join
 from map import Map
+from actor import Actor
 
 filename=join("maps","vasvill.txt")
 try:
@@ -15,3 +16,10 @@ for r in range(test_map.height):
     for c in range(test_map.width):
         print(test_map.get_field((r,c)), end='')
     print()
+
+
+test_actor = Actor()
+test_actor.set_map(test_map)
+test_actor.set_position((4,8))
+for direction in Actor.MOVES:
+    print(f"Direction {direction} is {'free' if test_actor.can_move(direction) else 'unavailable'}")
